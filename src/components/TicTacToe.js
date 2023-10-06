8a5819acf3e3e87a0b092cb583d859e694a32dbb
Swapnali Waghunde <swapnaliwaghunde@Swapnalis-MacBook-Pro.local>
2023-10-06 15:50:07 +0800

    tic tac toe basic frontend

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,5 +1,37 @@
+import classes from "./TicTacToe.module.css";
 const TicTacToe = () => {
-  return <h1>Tic Tac Toe</h1>;
+  const resetHandler = () => {
+    console.log("reset");
+  };
+  return (
+    <div className={classes.game}>
+      <div className={classes.gameBoard}>
+        <div className={classes.status}>
+          <p>Next player: X</p>
+        </div>
+        <div className={classes.board}>
+          <div className={classes.boardRow}>
+            <button className={classes.square}></button>
+            <button className={classes.square}></button>
+            <button className={classes.square}></button>
+          </div>
+          <div className={`${classes.boardRow} ${classes.rowRowMiddle}`}>
+            <button className={classes.square}></button>
+            <button className={classes.square}></button>
+            <button className={classes.square}></button>
+          </div>
+          <div className={classes.boardRow}>
+            <button className={classes.square}></button>
+            <button className={classes.square}></button>
+            <button className={classes.square}></button>
+          </div>
+        </div>
+        <button className={classes.resetBtn} onClick={resetHandler}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TicTacToe;
